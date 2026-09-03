0d246c0d00a27f0ae3627929fd4c2e92966cf238
Viraj Mulik <144205690+Viraj1923@users.noreply.github.com>
2026-09-03 14:45:03 +0530

    Time: 18 ms (13.57%), Space: 46.7 MB (16.24%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map=new HashMap<>();
+
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            if(!map.containsKey(ch)){
+               return false; 
+            }
+            map.put(ch,map.get(ch)-1);
+            if(map.get(ch)==0){
+                map.remove(ch);
+            }
+        }
+
+        return map.isEmpty();
+    }
+}
