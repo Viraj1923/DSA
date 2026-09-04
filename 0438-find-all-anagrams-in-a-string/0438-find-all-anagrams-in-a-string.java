ab4870971b110c78d32d5733aece3d96f1a1af66
Viraj Mulik <144205690+Viraj1923@users.noreply.github.com>
2026-09-04 15:35:55 +0530

    Time: 716 ms (7.6%), Space: 47.3 MB (27.46%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,20 +1,17 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> list = new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
+        int l=0;
+        int r=p.length()-1;
 
-        int l = 0;
-        int r = p.length() - 1;
-
-        while (r < s.length()) {
-
-            if (isAnagram(s, p, l, r)) {
+        while(r<s.length()){
+            if(isAnagram(s,p,l,r)){
                 list.add(l);
             }
-
             l++;
             r++;
+            
         }
-
         return list;
     }
 
@@ -37,4 +34,26 @@ class Solution {
 
         return true;
     }
+
+    // public static boolean isAnagram(String str,String p){
+    //     if(str.length()!=p.length()) return false;
+        
+    //     HashMap<Character,Integer> map=new HashMap<>();
+    //     for(char ch:str.toCharArray()){
+    //         map.put(ch,map.getOrDefault(ch,0)+1);
+    //     }
+
+    //     for(int i=0;i<p.length();i++){
+    //         char ch=p.charAt(i);
+    //         if(!map.containsKey(ch)){
+    //             return false;
+    //         }
+    //         map.put(ch,map.get(ch)-1);
+    //         if(map.get(ch)==0){
+    //             map.remove(ch);
+    //         }
+    //     }
+    //     return map.isEmpty();
+
+    // }
 }
